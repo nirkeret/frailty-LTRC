f409bd1b398e144acf224d660da3bff79561314b
nirkeret <50802985+nirkeret@users.noreply.github.com>
2020-08-02 17:38:04 +0300

    Update Sampling from gamma.R

--- Sampling from gamma.R
+++ Sampling from gamma.R
@@ -44,7 +44,7 @@ H023_real <- function(t)
 ######## creating reference data - mimicking the marginal death in the population below recL - 
 
 nnn <- 100000
-z1 <- runif(nn) ; z2 <- runif(nn) ; z3 <- runif(nn)
+z1 <- runif(nnn) ; z2 <- runif(nnn) ; z3 <- runif(nnn)
 z <- cbind(z1,z2,z3)
 
 omega_ref <- rgamma(nnn,shape = 1/theta,scale = theta)
